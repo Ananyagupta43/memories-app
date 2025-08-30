37ba60ceef94348d0da78c67f6c641d6a5471b3b
Ananya <gananya320@gmail.com>
2025-08-30 16:42:20 +0530

    Added basic template for app

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from 'react-dom/client';
+
+const App=()=>{
+    return <div>Heading</div>
+}
+
+const root=ReactDom.createRoot(document.getElementById('root'));
+
+root.render(<App/>)
